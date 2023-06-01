c547423182eca00c55a2710e31582b937c04c3a4
panahlimurad <panahli.murad@gmail.com>
2023-06-02 01:53:40 +0400

    end calculate page

--- assens/calc.js
+++ assens/calc.js
@@ -1,3 +1,71 @@
 $(document).ready(function () {
+ 
+    let firstNumber = ""
+    let secondNumber = ""
+    let operator = ""
+
+
+    function printButton() {
+        let btn = $(this)
+        let text = $(this).text()
+        
+      if (btn.hasClass("operator")) {
+            operator = text
+          $(".show-calc").html(firstNumber + operator + secondNumber)
+      } 
+      else {
+          if (operator === "") {
+            firstNumber += text
+            $(".show-calc").html(firstNumber + operator + secondNumber);            
+          } else {
+            secondNumber += text
+            $(".show-calc").html(firstNumber + operator + secondNumber);          
+          }
+
+        }
+    }
     
-})
+    function printResult() {
+        let result;
+
+        switch (operator) {
+          case "+":
+            result = +firstNumber + +secondNumber;
+            break;
+          case "-":
+            result = +firstNumber - +secondNumber;
+            break;
+          case "×":
+            result = +firstNumber * +secondNumber;
+            break;
+          case "÷":
+            result = +firstNumber / +secondNumber;
+            break;
+          case "√":
+            result = Math.sqrt(+firstNumber);
+            break;
+          case "%":
+            result = (+firstNumber * +secondNumber) / 100;
+            break;
+          case "+/-":
+            result = -1 * +firstNumber;
+            break;
+        }
+
+        $(".show-result").html(result)
+
+    }
+
+    function removeDisplay(){
+        firstNumber = "";
+        secondNumber = "";
+        operator = "";
+        $(".show-calc").html("");
+        $(".show-result").html("");
+    }
+
+    $(document).on("click", ".count", printButton);
+    $(document).on("click", ".equal", printResult);
+    $(document).on("click", ".remove", removeDisplay);
+
+});
